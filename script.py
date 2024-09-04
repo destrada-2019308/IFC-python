77f04c57df97972603f4dd16a50a2c6f5f7e283e
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2024-09-04 17:05:32 -0600

    Exportando datos a JSON

---
+++ script.py
@@ -0,0 +1,42 @@
+import ifcopenshell
+import ifcopenshell.util.element as Element
+
+model = ifcopenshell.open('./modal/IFC-2-1.ifc')
+
+#CREAMOS FUNCIÓN PARA FILTRAR POR TIPO
+def allData (model, type):
+    #ARREGLO VACÍO
+    data = []
+    #EL FILTRO LO GUARDAMOS EN objects
+    objects = model.by_type(type)
+
+    for object in objects:
+        #GUARDAMOS CADA UNO DE LOS ID´S OBJECT 
+        id = object.id()
+        #EL APPEND NOS CREA LOS OBJETOS AL FINAL DE LA LISTA - EN ESTE CASO data QUE ES UN ARRAY -
+        data.append({
+            "ExpressID": object.id(),
+            "GlobalId": object.GlobalId,
+            "Class": object.is_a(),
+            "PredefinedType": Element.get_predefined_type(object),
+            "Name": object.Name,
+            "Level": Element.get_container(object).Name
+            if Element.get_container(object)
+                else  "",
+            "ObjectType": Element.get_type(object).Name
+            if Element.get_type(object)
+                else "",
+            "QuantitySets": Element.get_psets(object, qtos_only=True),
+            "PropertySets": Element.get_psets(object, psets_only=True),
+            "FIN": "LOREM IPSUM"
+        })
+    return data
+
+import pprint
+
+pp = pprint.PrettyPrinter()
+datos = allData(model, 'IfcBuildingElement')
+
+import json
+with open('./modal/test.json', 'w') as f: 
+    json.dump(datos, f)
---
+++ test.py
@@ -0,0 +1,30 @@
+import ifcopenshell
+import ifcopenshell.util.element
+model = ifcopenshell.open('./modal/IFC-2-1.ifc')
+
+#print(model.schema)  
+# USA EL IFC2X3 
+walls = model.by_type('IfcWall') 
+#wall = model.by_type('IfcWall')[0]
+  
+data = {}
+
+#FOR para recorrer todos los arreglos 
+for wall in range(len(walls)):
+    print(wall)   
+    wall1 = model.by_type('IfcWall')[wall]
+    print(wall1)
+    print("----------------------------------------")
+    print(ifcopenshell.util.element.get_psets(wall1))
+
+
+
+"""
+print(model.by_id(1))
+
+walls = model.by_type('IfcWall')
+
+print(walls[0])
+
+print(len(walls))
+"""
